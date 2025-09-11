a69267c4301b4a97aeefd23dc6c72c549564c0f1
4rdnac <candraahmaddani04@gmail.com>
2025-09-11 16:03:16 +0700

    revisi tugas

--- codelab03_dart/src/TugasPraktikum.dart
+++ codelab03_dart/src/TugasPraktikum.dart
@@ -2,15 +2,17 @@ void main() {
   String nama = "Candra Ahmad Dani";
   String nim = "2341720187";
 
-  for (int i = 0; i <= 201; i++) {
-    if (isPrima(i)) {
-      print("$i adalah bilangan prima $nama | $nim");
+  for (int n = 0; n <= 201; n++) {
+    if (isPrima(n)) {
+      print("$n -> $nama | $nim");
+    } else {
+      print(n);
     }
   }
 }
 
 bool isPrima(int n) {
-  if (n < 2) return false;
+  if (n < 2) return false;  
   for (int i = 2; i * i <= n; i++) {
     if (n % i == 0) return false;
   }
