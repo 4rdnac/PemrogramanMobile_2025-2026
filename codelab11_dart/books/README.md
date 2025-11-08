5c9f14a859d0303067fb5ee5f7cbeaf07f5aaf56
4rdnac <candraahmaddani04@gmail.com>
2025-11-08 19:08:05 +0700

    W11: Soal 3

--- codelab11_dart/books/README.md
+++ codelab11_dart/books/README.md
@@ -1,4 +1,13 @@
 # books
+
 Soal 2
 
 ![Output](assets/01.png)
+
+Soal 3
+
+Jelaskan maksud kode langkah 5 tersebut terkait `substring` dan `catchError`!
+
+substring(0, 450) membatasi tampilan hasil hanya 450 karakter pertama, sedangkan catchError menangani kesalahan saat pengambilan data dan menampilkan pesan “An error occurred”.
+
+![Output](assets/02.gif)
--- codelab11_dart/books/lib/main.dart
+++ codelab11_dart/books/lib/main.dart
@@ -42,9 +42,21 @@ class _FuturePageState extends State<FuturePage> {
           children: [
             const Spacer(),
             ElevatedButton(
-              child: const Text('GO!'),
-              onPressed: () {},
-              ),
+              child: Text('GO!'),
+              onPressed: () {
+                setState(() {});
+                getData()
+                    .then((value) {
+                      result = value.body.toString().substring(0, 450);
+                      setState(() {});
+                    })
+                    .catchError((_) {
+                      result = 'An error occurred';
+                      setState(() {});
+                    });
+              },
+            ),
+
             const Spacer(),
             Text(result),
             const Spacer(),
@@ -52,7 +64,7 @@ class _FuturePageState extends State<FuturePage> {
             const Spacer(),
           ],
         ),
-      ),    
+      ),
     );
   }
 
